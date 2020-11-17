4240ac524340c7a95ab5371635e882bc46a5c0a1
DivineJK <66942837+DivineJK@users.noreply.github.com>
2020-11-17 17:56:36 +0900

    Move fracs to Rational.py

--- MathLibrary/IntLib.py
+++ MathLibrary/IntLib.py
@@ -243,33 +243,6 @@ class IntLib:
                 res[i] %= MOD
         return res
     
-    def frac_sum(self, f1: list, f2: list, weight=1):
-        if f1[1] == 0 or f2[1] == 0:
-            raise ValueError("Division by zero. f1 = {}/{}, f2 = {}/{}".format(f1[0], f1[1], f2[0], f2[1])) from None
-        f = [f1[0]*f2[1]+weight*f2[0]*f1[1], f1[1]*f2[1]]
-        g = self.gcd(f[0], f[1])
-        f[0] //= g
-        f[1] //= g
-        if f[1] < 0:
-            f[0] = -f[0]
-            f[1] = -f[1]
-        return f
-    
-    def frac_prod(self, f1: list, f2: list, inverse=False):
-        if inverse:
-            f = [f1[0]*f2[1], f1[1]*f2[0]]
-        else:
-            f = [f1[0]*f2[0], f1[1]*f2[1]]
-        if f[1] == 0:
-            raise ValueError("Division by zero. f1 = {}/{}, f2 = {}/{}".format(f1[0], f1[1], f2[0], f2[1])) from None
-        g = self.gcd(f[0], f[1])
-        f[0] //= g
-        f[1] //= g
-        if f[1] < 0:
-            f[0] = -f[0]
-            f[1] = -f[1]
-        return f
-    
     def get_bernouill(self, n, modulo):
         if modulo <= 0 or not self.IsPrime(modulo):
             raise RuntimeError("modulo value is invalid") from None
