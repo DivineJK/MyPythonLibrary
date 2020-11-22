e55989a92d2f9803c592b117f43a08b174791790
DivineJK <66942837+DivineJK@users.noreply.github.com>
2020-11-22 20:38:06 +0900

    Create ForStealth.py

---
+++ Others/ForStealth.py
@@ -0,0 +1,7 @@
+for i in range(6):
+    print("----- INPUT FOR A_C___{} -----".format(chr(i+65)))
+print("----- OTHER INPUTS -----")
+for i in range(6):
+    print("input()")
+    print("print('----- OUTPUT FOR A_C___{} -----')".format(chr(i+65)))
+    print("# A_C___{}".format(chr(i+65)))
