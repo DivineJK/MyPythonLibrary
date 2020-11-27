c85b9528eeabd823f4c391d79317fea91029f108
DivineJK <66942837+DivineJK@users.noreply.github.com>
2020-11-27 21:34:46 +0900

    Create FenwickTree.py

---
+++ DataStructures/FenwickTree.py
@@ -0,0 +1,21 @@
+class fenwick_tree:
+    def __init__(self, n, initial=[]):
+        self.n = n
+        self.ft = [0]*(n+1)
+        if initial != []:
+            for i in range(n):
+                x = i + 1
+                while x <= n:
+                    self.ft[x] += initial[i]
+                    x += x - (x & (x - 1))
+    def ft_add(self, p, v):
+        x = p + 1
+        while x <= self.n:
+            self.ft[x] += v
+            x += x - (x & (x - 1))
+    def ft_sum(self, x):
+        S = 0
+        while x:
+            S += self.ft[x]
+            x = x & (x - 1)
+        return S
