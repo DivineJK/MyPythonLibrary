0df904ec1f4136be464986a32334350ab49cc9aa
DivineJK <66942837+DivineJK@users.noreply.github.com>
2020-12-13 18:40:08 +0900

    Update ForStealth.py

--- Others/ForStealth.py
+++ Others/ForStealth.py
@@ -1,10 +1,14 @@
-for i in range(6):
-    print("----- INPUT FOR A_C___{} -----".format(chr(i+65)))
+contest_name = ""
+problem_num = 6
+for i in range(problem_num):
+    print("----- INPUT FOR {}{} -----".format(contest_name, chr(i+65)))
 print("----- OTHER INPUTS -----")
-for i in range(6):
+for i in range(problem_num):
     print("input()")
-    print("print('----- OUTPUT FOR A_C___{} -----')".format(chr(i+65)))
-    print("# A_C___{}".format(chr(i+65)))
+    print("print('----- OUTPUT FOR {}{} -----')".format(contest_name, chr(i+65)))
+    print("# {}{}".format(contest_name, chr(i+65)))
+    print()
+    print()
 print("input()")
-print("----- OTHER OUTPUTS -----")
+print("print('----- OTHER OUTPUTS -----')")
 print("# OTHER CODE")
