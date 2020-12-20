0cd3828bbd37a82ec3e870436445664d05b5ced3
DivineJK <66942837+DivineJK@users.noreply.github.com>
2020-12-20 23:13:37 +0900

    Update Convolution_gcd.py

--- MathLibrary/Convolution_gcd.py
+++ MathLibrary/Convolution_gcd.py
@@ -4,13 +4,16 @@
 # C[k] = sum(A[i]*B[j] for {i, j | gcd(i, j) = k})
 
 for i in range(N):
-    for j in range(2*(i+1), M+1, i+1):
+    for j in range(2*(i+1), N+1, i+1):
         A[i] += A[j-1]
 for i in range(M):
     for j in range(2*(i+1), M+1, i+1):
         B[i] += B[j-1]
-C = [A[i]*B[i]%mod for i in range(M)]
-for i in range(M, 0, -1):
-    for j in range(2*i, M+1, i):
+if N < L:
+    A = A[:] + [0]*(L-N)
+if M < L:
+    B = B[:] + [0]*(L-M)
+C = [A[i]*B[i] for i in range(L)]
+for i in range(L, 0, -1):
+    for j in range(2*i, L+1, i):
         C[i-1] -= C[j-1]
-        C[i-1] %= mod
