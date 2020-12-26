453bb9103425cf4bc89cc922770e113a9f4c912b
DivineJK <66942837+DivineJK@users.noreply.github.com>
2020-12-26 09:37:06 +0900

    Update Convolution_xor.py

--- MathLibrary/Convolution_xor.py
+++ MathLibrary/Convolution_xor.py
@@ -1,6 +1,6 @@
 def conv_xor(n, f, g):
     if n == 1:
-        return [f[0]*g[0]%mod]
+        return [f[0]*g[0]]
     f0 = [f[i] for i in range(n//2)]
     f1 = [f[i] for i in range(n//2, n)]
     g0 = [g[i] for i in range(n//2)]
@@ -9,4 +9,4 @@ def conv_xor(n, f, g):
     x01 = conv_xor(n//2, f0, g1)
     x10 = conv_xor(n//2, f1, g0)
     x11 = conv_xor(n//2, f1, g1)
-    return [(x00[i]+x11[i])%mod for i in range(n//2)] + [(x01[i]+x10[i])%mod for i in range(n//2)]
+    return [x00[i]+x11[i] for i in range(n//2)] + [x01[i]+x10[i] for i in range(n//2)]
