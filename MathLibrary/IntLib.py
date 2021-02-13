5748606edb19da254eb33cfff6e7bca010f438f0
DivineJK <66942837+DivineJK@users.noreply.github.com>
2021-02-13 16:11:29 +0900

    Update IntLib.py

--- MathLibrary/IntLib.py
+++ MathLibrary/IntLib.py
@@ -97,3 +97,24 @@ def totient(n):
             res *= a - 1
             break
     return res
+def doubling(p, l, modulo=0):
+    res = 0
+    bas = 1
+    if modulo:
+        bas = pow(10, p, modulo)
+    else:
+        bas = pow(10, p)
+    rep_unit = 1
+    while l:
+        if l & 1:
+            res *= bas
+            res += rep_unit
+            if modulo:
+                res %= modulo
+        rep_unit = (bas + 1) * rep_unit
+        bas *= bas
+        if modulo:
+            rep_unit %= modulo
+            bas %= modulo
+        l >>= 1
+    return res
