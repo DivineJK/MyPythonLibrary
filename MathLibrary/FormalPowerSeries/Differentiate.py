2f24349d5759b3caf36ca9a25d890ba72dab16be
DivineJK <66942837+DivineJK@users.noreply.github.com>
2021-02-17 19:25:57 +0900

    Update Differentiate.py

--- MathLibrary/FormalPowerSeries/Differentiate.py
+++ MathLibrary/FormalPowerSeries/Differentiate.py
@@ -1,6 +1,6 @@
-def differentiate(f, modulo):
+def differentiate(f, MOD=MOD_free):
     n = len(f)
     res = [0]*n
     for i in range(1, n):
-        res[i-1] = i * f[i] % modulo
+        res[i-1] = i * f[i] % MOD
     return res
