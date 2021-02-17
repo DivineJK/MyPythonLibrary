79410ca95e92d46ce77964b33ec73821610c6810
DivineJK <66942837+DivineJK@users.noreply.github.com>
2021-02-17 18:25:01 +0900

    Update and rename NTT_for_FPS.py to NTT_friendly.py

---
+++ MathLibrary/FormalPowerSeries/NTT_friendly.py
@@ -0,0 +1,71 @@
+# mod = 998244353, root = 31, inverse_root = 128805723
+MOD_free = 998244353
+root_free = 128805723
+invr_free = 31
+def ntt_free(f, n, root=root_free):
+    if n == 1:
+        return f
+    MOD = MOD_free
+    depth = len(bin(n))-3
+    res = [0]*n
+    pos = 0
+    for i in range(n):
+        res[i] = f[pos]
+        tmp = ((i+1)&-(i+1)) << 1
+        pos ^= (n-1)&(n-n//tmp)
+    left = 2
+    right = 1
+    thgir = n >> 1
+    base_list = [1]*24
+    base_list[-1] = root
+    for i in range(23, 0, -1):
+        base_list[i-1] = base_list[i] * base_list[i] % MOD
+    for i in range(depth):
+        grow = 1
+        seed = base_list[i+1]
+        for k in range(right):
+            idx_l = k
+            idx_r = k + right
+            for j in range(thgir):
+                u = res[idx_l]
+                v = res[idx_r] * grow % MOD
+                res[idx_l] = (u + v) % MOD
+                res[idx_r] = (u - v) % MOD
+                idx_l += left
+                idx_r += left
+            grow = grow * seed % MOD
+        left <<= 1
+        right <<= 1
+        thgir >>= 1
+    return res
+def inverse_ntt_free(f, n):
+    res = ntt_free(f, n, invr_free)
+    MOD = MOD_free
+    x, y, u, v, k, l = 1, 0, 0, 1, n, MOD
+    while l:
+        x, y, u, v, k, l = u, v, x - u * (k // l), y - v * (k // l), l, k % l
+    x %= MOD
+    for i in range(n):
+        res[i] *= x
+        res[i] %= MOD
+    return res
+def convolute_one(f, g, MOD=MOD_free):
+    n = len(f)
+    m = len(g)
+    bin_top = 1
+    while bin_top < n + m:
+        bin_top <<= 1
+    if n * m <= 100000:
+        res = [0]*bin_top
+        for i in range(n):
+            for j in range(m):
+                res[i+j] += (f[i] * g[j]) % MOD
+                res[i+j] %= MOD
+        return res
+    x = f[:] + [0]*(bin_top-n)
+    y = g[:] + [0]*(bin_top-m)
+    x = ntt0(x, bin_top)
+    y = ntt0(y, bin_top)
+    for i in range(bin_top):
+        x[i] = x[i] * y[i] % MOD
+    return inverse_ntt0(x, bin_top)
