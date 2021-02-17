fa08a81efaa510261d99fcd269fd8b33a56355f3
DivineJK <66942837+DivineJK@users.noreply.github.com>
2021-02-17 21:38:51 +0900

    Update BernoulliNumber_Normal.py

--- MathLibrary/BernoulliNumber_Normal.py
+++ MathLibrary/BernoulliNumber_Normal.py
@@ -1,11 +1,11 @@
-class bernouill_normal:
+class bernoulli_normal:
     def __init__(self):
         self.prev_mod = 0
         self.fact_cnt = 0
         self.fact = [1]
         self.invf = [1]
-        self.bernouill_cnt = 0
-        self.bernouill = [1]
+        self.bernoulli_cnt = 0
+        self.bernoulli = [1]
     def inved(self, a, modulo):
         x, y, u, v, k, l = 1, 0, 0, 1, a, modulo
         while l:
@@ -16,8 +16,8 @@ class bernouill_normal:
             self.fact_cnt = 0
             self.fact = [1]
             self.invf = [1]
-            self.bernouill_cnt = 0
-            self.bernouill = [1]
+            self.bernoulli_cnt = 0
+            self.bernoulli = [1]
         self.prev_mod = modulo
         if n > self.fact_cnt:
             self.fact = self.fact[:] + [0]*(n-self.fact_cnt)
@@ -32,32 +32,32 @@ class bernouill_normal:
                     self.invf[i-1] = self.invf[i] * i
                     self.invf[i-1] %= modulo
             self.fact_cnt = n
-    def get_bernouill(self, n, modulo):
+    def get_bernoulli(self, n, modulo):
         if n+1 > self.fact_cnt:
             self.make_fact(n+1, modulo)
-        if n > self.bernouill_cnt:
-            for i in range(self.bernouill_cnt, n):
-                self.bernouill.append(0)
-            for i in range(self.bernouill_cnt+1, n+1):
+        if n > self.bernoulli_cnt:
+            for i in range(self.bernoulli_cnt, n):
+                self.bernoulli.append(0)
+            for i in range(self.bernoulli_cnt+1, n+1):
                 if i % 2 == 0 or i == 1:
                     for j in range(i):
                         if j == 1 or j % 2 == 0:
                             tmp = self.invf[i-j+1]*self.invf[j] % modulo
-                            self.bernouill[i] += self.bernouill[j]*tmp%modulo
-                            self.bernouill[i] %= modulo
-                    self.bernouill[i] *= -self.fact[i]
-                    self.bernouill[i] %= modulo
-            self.bernouill_cnt = n
-        return self.bernouill[n]
+                            self.bernoulli[i] += self.bernoulli[j]*tmp%modulo
+                            self.bernoulli[i] %= modulo
+                    self.bernoulli[i] *= -self.fact[i]
+                    self.bernoulli[i] %= modulo
+            self.bernoulli_cnt = n
+        return self.bernoulli[n]
     def power_sum(self, n, m, modulo):
         bas = n
         S = 0
         sign = 1 - 2 * (m % 2)
         for i in range(m+1):
-            if m-i > self.bernouill_cnt:
-                B = self.get_bernouill(m-i, modulo)
+            if m-i > self.bernoulli_cnt:
+                B = self.get_bernoulli(m-i, modulo)
             else:
-                B = self.bernouill[m-i]
+                B = self.bernoulli[m-i]
             S += (self.invf[i+1]*self.invf[m-i]%modulo)*(sign*B*bas%modulo)%modulo
             S %= modulo
             bas *= n
