a788975ce33579c34a8c490aca55bb9b33508c36
DivineJK <66942837+DivineJK@users.noreply.github.com>
2021-02-18 15:29:24 +0900

    Update FHT.py

--- MathLibrary/FHT.py
+++ MathLibrary/FHT.py
@@ -1,24 +1,51 @@
-def inved(a):
-    x, y, u, v, k, l = 1, 0, 0, 1, a, mod
+def inved(a, modulo):
+    x, y, u, v, k, l = 1, 0, 0, 1, a, modulo
     while l:
         x, y, u, v, k, l = u, v, x - u * (k // l), y - v * (k // l), l, k % l
-    return x % mod
-def fht(f, n, inverse=False):
+    return x % modulo
+def fht(f, n, MOD):
     if n == 1:
         return f
-    if n == 2:
-        ipl = inved(2)
-        if inverse:
-            return [ipl * (f[0] + f[1]) % mod, ipl * (f[0] - f[1]) % mod]
-        return [(f[0] + f[1]) % mod, (f[0] - f[1]) % mod]
-    f0 = [f[i] for i in range(n//2)]
-    f1 = [f[i] for i in range(n//2, n)]
-    x0 = fht(f0, n//2)
-    x1 = fht(f1, n//2)
-    x = [(x0[i]+x1[i]) % mod for i in range(n//2)] + [(x0[i]-x1[i]) % mod for i in range(n//2)]
-    if inverse:
-        ipl = inved(n)
-        for i in range(n):
-            x[i] *= ipl
-            x[i] %= mod
+    depth = len(bin(n))-3
+    res = [0]*n
+    pos = 0
+    for i in range(n):
+        res[i] = f[pos]
+        tmp = ((i+1)&-(i+1)) << 1
+        pos ^= (n-1)&(n-n//tmp)
+    left = 2
+    right = 1
+    thgir = n >> 1
+    for i in range(depth):
+        grow = 1
+        for k in range(right):
+            idx_l = k
+            idx_r = k + right
+            for j in range(thgir):
+                u = res[idx_l]
+                v = res[idx_r]
+                res[idx_l] = (u + v) % MOD
+                res[idx_r] = (u - v) % MOD
+                idx_l += left
+                idx_r += left
+        left <<= 1
+        right <<= 1
+        thgir >>= 1
+    return res
+def xor_convolution(f, g, MOD):
+    n = len(f)
+    m = len(f)
+    bin_top = 1
+    while bin_top < max(n, m):
+        bin_top <<= 1
+    x = f[:] + [0]*(bin_top-n)
+    y = g[:] + [0]*(bin_top-m)
+    x = fht(x, bin_top, MOD)
+    y = fht(y, bin_top, MOD)
+    for i in range(bin_top):
+        x[i] = x[i] * y[i] % MOD
+    x = fht(x, bin_top, MOD)
+    ib = inved(bin_top, MOD)
+    for i in range(bin_top):
+        x[i] = x[i] * ib % MOD
     return x
