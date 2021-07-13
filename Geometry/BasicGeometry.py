56cecb5578ff2f4702ee41bf8d87e2e6a2f50dbc
DivineJK <66942837+DivineJK@users.noreply.github.com>
2021-07-13 23:25:52 +0900

    Update BasicGeometry.py

--- Geometry/BasicGeometry.py
+++ Geometry/BasicGeometry.py
@@ -8,7 +8,7 @@ def dist(p1, p2, squared=False):
     if squared: return res
     return res**0.5
 
-def intersection_point(p1, p2, r):
+def intersection_point_same_radius(p1, p2, r):
     if p1 == p2: return (p1[0] + r, p1[1])
     t = dist(p1, p2, squared=True)
     st = dist(p1, p2)
@@ -19,3 +19,23 @@ def intersection_point(p1, p2, r):
     Y1 = (p1[0] - p2[0])*h*st/(2*t) + g[1]
     Y2 = (p2[0] - p1[0])*h*st/(2*t) + g[1]
     return (X1, Y1), (X2, Y2)
+
+def intersection_point(p1, p2, r1, r2):
+    if p1 == p2:
+        if r1 == r2:
+            return (p1[0] + r1, p1[1])
+        return None, None
+    if dist(p1, p2, squared=True) > (r1 + r2) * (r1 + r2):
+        return None, None
+    if dist(p1, p2, squared=True) < (r1 - r2) * (r1 - r2):
+        return None, None
+    dd = dist(p1, p2, squared=True)
+    kk = (4*r1*r1*dd - (r1*r1-r2*r2+dd)*(r1*r1-r2*r2+dd))/(4*dd*dd)
+    if kk < 0:
+        return None, None
+    k = pow(kk, 0.5)
+    mm = (r1*r1 - r2*r2 + dd) / (2*dd)
+    px = (r1*r1-r2*r2)/(2*dd) * (p2[0] - p1[0]) + (p2[0] + p1[0]) / 2
+    py = (r1*r1-r2*r2)/(2*dd) * (p2[1] - p1[1]) + (p2[1] + p1[1]) / 2
+    p = (px, py)
+    return (px - k * (p2[1] - p1[1]), py + k * (p2[0] - p1[0])), (px + k * (p2[1] - p1[1]), py - k * (p2[0] - p1[0]))
