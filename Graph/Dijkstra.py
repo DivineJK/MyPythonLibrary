fb3a876a8d82624d601c50f8a538e3882a888c28
DivineJK <66942837+DivineJK@users.noreply.github.com>
2021-07-18 19:11:29 +0900

    Update Dijkstra.py

--- Graph/Dijkstra.py
+++ Graph/Dijkstra.py
@@ -1,60 +1,40 @@
 from heapq import heappush, heappop
-class dijkstra:
-    def __init__(self, n, identity=0, INF=int(1e18)):
-        self.INF = INF
+class Dijkstra:
+    def __init__(self, n):
         self.n = n
-        self.identity = identity
-        self.cost = [INF]*n
-        self.neighbor = [[] for _ in range(n)]
-        self.neighbor_cost = {}
-        self.prev = [-1]*n
-    def reset(self):
-        self.cost = [INF]*self.n
-        self.prev = [-1]*self.n
-        self.neighbor = [[] for _ in range(self.n)]
-        self.neighbor_cost = {}
-    def connect_edge(self, a, b, c=1, directed=True):
-        if a*self.n+b not in self.neighbor_cost:
-            self.neighbor[a].append(b)
-            self.neighbor_cost[a*self.n+b] = c
-        else:
-            if self.neighbor_cost[a*self.n+b] > c:
-                self.neighbor_cost[a*self.n+b] = c
-        if not directed:
-            if b*self.n+a not in self.neighbor_cost:
-                self.neighbor[b].append(a)
-                self.neighbor_cost[b*self.n+a] = c
-            else:
-                if self.neighbor_cost[b*self.n+a] > c:
-                    self.neighbor_cost[b*self.n+a] = c
-    def simply_dijkstra(self, st):
-        pq = []
-        pq_cnt = 0
-        self.cost[st] = self.identity
-        for i in range(self.n):
-            pq_cnt += 1
-            heappush(pq, (self.cost[i], i))
+        self.graph = [[] for _ in range(self.n)]
+        self.inf = int(1e18)+18
+        self.threshold = int(1e18)
+        self.cost = [self.inf]*self.n
+        self.isDirty = False
+    def resetAll(self):
+        self.graph = [[] for _ in range(self.n)]
+        self.cost = [self.inf]*self.n
+        self.isDirty = False
+    def resetOnlyCost(self):
+        self.cost = [self.inf]*self.n
+        self.isDirty = False
+    def connectEdge(self, a, b, c, undirected=True):
+        self.graph[a].append((b, c))
+        if undirected:
+            self.graph[b].append((a, c))
+    def getMinimumCost(self, start):
+        if self.isDirty:
+            for i in range(self.n):
+                self.cost[i] = self.inf
+        self.cost[start] = 0
+        pq = [(0, start)]
+        pq_cnt = 1
         while pq_cnt:
             b = heappop(pq)
             r = b[1]
             pq_cnt -= 1
             if self.cost[r] < b[0]:
                 continue
-            for i in self.neighbor[r]:
-                if self.cost[i] > self.cost[r]+self.neighbor_cost[r*self.n+i]:
-                    self.cost[i] = self.cost[r]+self.neighbor_cost[r*self.n+i]
-                    self.prev[i] = r
-                    heappush(pq, (self.cost[i], i))
+            for k in self.graph[r]:
+                i, v = k[0], k[1]
+                if self.cost[i] > self.cost[r] + v:
+                    self.cost[i] = self.cost[r] + v
                     pq_cnt += 1
-    def path_restore(self, s, g):
-        path = [g]
-        if s == g:
-            return path
-        while self.prev[g] != s:
-            if self.prev[g] == -1:
-                return []
-            g = self.prev[g]
-            path.append(g)
-        path.append(s)
-        path.reverse()
-        return path
+                    heappush(pq, (self.cost[i], i))
+        self.isDirty = True
