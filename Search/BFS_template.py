3b35194b6812003e2a1f6ce2e7a078add1b3fdbd
DivineJK <66942837+DivineJK@users.noreply.github.com>
2021-09-18 13:28:14 +0900

    Update BFS_template.py

--- Search/BFS_template.py
+++ Search/BFS_template.py
@@ -16,3 +16,23 @@ while pnt < cnt:
                 # st.append()
                 cnt += 1
     cnt = pnter
+
+"""
+# AGC019F - Yes or No
+
+N, M = map(int, input().split())
+mod = 998244353
+def gcd(x, y):
+    while y:
+        x, y = y, x % y
+    return x
+dp = [[0]*(M+1) for i in range(N+1)]
+for i in range(N+1):
+    dp[i][0] = i
+for i in range(M+1):
+    dp[0][i] = i
+for i in range(N):
+    for j in range(M):
+        dp[i+1][j+1] = ((i+1)*dp[i][j+1]+(j+1)*dp[i+1][j]+max(i+1, j+1))*pow(i+j+2, mod-2, mod)%mod
+print(dp[N][M])
+"""
