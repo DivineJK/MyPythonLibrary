dcf6971fc9f6843760a7b674c314182a6d41d989
DivineJK <66942837+DivineJK@users.noreply.github.com>
2021-11-22 00:33:25 +0900

    Update CRT.py

--- MathLibrary/Integer/CRT.py
+++ MathLibrary/Integer/CRT.py
@@ -6,3 +6,22 @@ def CRT(num, a_list, m_list):
         r += bas * x
         bas *= m_list[i]
     return r % bas
+def garner(a, m, mod = 0):
+    n = len(a)
+    c = [0]*n
+    b = 1
+    res = 0
+    for i in range(n):
+        t = a[i]
+        if a[i] >= m[i]:
+            a[i] %= m[i]
+        for j in range(i):
+            t -= c[j]
+            t = (t * single_modinv(m[j], m[i])) % m[i]
+        res += b * t
+        b *= m[i]
+        c[i] = t
+        if mod:
+            res %= mod
+            b %= mod
+    return res
