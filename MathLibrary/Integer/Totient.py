770752205e75085f086369b9fd9b8cb92dfd411a
DivineJK <66942837+DivineJK@users.noreply.github.com>
2021-11-22 19:50:03 +0900

    Update Totient.py

--- MathLibrary/Integer/Totient.py
+++ MathLibrary/Integer/Totient.py
@@ -1,16 +1,23 @@
 def totient(n):
     a = n
-    p = 2
-    res = n
+    res = 1
+    q = 1
+    if a & 1 == 0:
+        q *= 2
+        while a & 1 == 0:
+            a >>= 1
+    p = 3
     while a != 1:
         if a % p == 0:
-            res //= p
+            u = 1
+            q *= p
             res *= p - 1
-            while a % p == 0:
-                a //= p
-        p += 1
+            while a % (u*p) == 0:
+                u *= p
+            a //= u
+        p += 2
         if p * p > n and a != 1:
-            res //= a
             res *= a - 1
+            q *= a
             break
-    return res
+    return res * n // q
