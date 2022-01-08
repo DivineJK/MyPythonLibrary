c6f1640914b25f0d6bdce86e60e7e2e022121c1a
DivineJK <66942837+DivineJK@users.noreply.github.com>
2022-01-08 17:16:37 +0900

    Update Convolution_prod.py

--- MathLibrary/Convolution/Convolution_prod.py
+++ MathLibrary/Convolution/Convolution_prod.py
@@ -1,3 +1,8 @@
-for i in range(1, 100001):
-    for j in range(1, 100000//i+1):
-        C[i*j-1] += A[i-1]*B[j-1]
+def convolution_prod(f, g):
+    n = len(f)
+    res = [0]*n
+    for i in range(n):
+        for j in range(n//(i+1)):
+            res[(i+1)*(j+1)-1] += f[i] * g[j]
+        print(n-(i+1)*(n//(i+1)) < i+1)
+    return res
