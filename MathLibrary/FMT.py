32c7a2a2e2a9992b386cd94be35372996c6f5d3b
DivineJK <66942837+DivineJK@users.noreply.github.com>
2022-01-08 15:49:01 +0900

    Update FMT.py

--- MathLibrary/FMT.py
+++ MathLibrary/FMT.py
@@ -1,24 +1,21 @@
 def FMT(f):
     n = len(f)
-    p_flg = [True]*(n+1)
-    p_flg[0] = False
-    p_flg[1] = False
-    p_tab = []
-    if n == 2:
-        p_tab.append(2)
-    else:
-        p = 3
-        while p * p <= n:
-            if p_flg[p]:
-                for i in range(p, n//p+1, 2):
-                    p_flg[p*i] = False
-            p += 1
-        p_tab = [2]
-        for i in range(3, n+1, 2):
-            if p_flg[i]:
-                p_tab.append(i)
-    res = [f[i] for i in range(n)]
-    for i in p_tab:
-        for j in range(n//i, 0, -1):
-            res[i*j-1] -= res[j-1]
-    return res
+    if n <= 1:
+        return f
+    flg = [True]*(n+1)
+    p = 3
+    while p * p <= n:
+        if flg[p]:
+            for i in range(p*p, n+1, p<<1):
+                flg[i] = False
+        p += 2
+    primes = [2]
+    for i in range(3, n+1, 2):
+        if flg[i]:
+            primes.append(i)
+    g = [f[i] for i in range(n)]
+    for i in primes:
+        t = n // i
+        for j in range(t, 0, -1):
+            g[i*j-1] -= g[j-1]
+    return g
